3612e807297f240adf6b0cdcdce713962d794c5b
Natalia Zherebtcova <natalya.zherebtsova@gmail.com>
2025-02-12 13:10:42 +0100

    2.1.4 slice

--- task_score 2.1.4.js
+++ task_score 2.1.4.js
@@ -5,5 +5,6 @@ let average = (introduction + git + js)/3
 let fullName = "Ivan Ivanov"
 let formattedName = fullName.slice(0,1) + "." + fullName.slice(5) 
 
+
 console.log(`${formattedName}: average score ${average}.`)
 
